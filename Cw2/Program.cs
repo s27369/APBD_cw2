25ec2aecae481a154fee77509eb3767b96c236ed
s27369 <s27369@pjwstk.edu.pl>
2024-03-24 20:19:31 +0100

    implemented feature-average

--- Cw2/Program.cs
+++ Cw2/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Cw2;
+﻿using System.Xml.XPath;
+
+namespace Cw2;
 
 class Program
 {
@@ -8,5 +10,14 @@ class Program
         Console.WriteLine("It's me");
         Console.WriteLine("s27369");
         Console.WriteLine(":D");
+        int[] tab = { 2, 4, 5, 7 };
+        Console.WriteLine(avg(tab));
+    }
+
+    public static double avg(int[] tab)
+    {
+        int sum = 0;
+        foreach (int v in tab) sum += v;
+        return (double)sum / tab.Length;
     }
 }
